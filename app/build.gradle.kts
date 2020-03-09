4ce947b963a82607771d1ffbf82634ae4c206a9b
MrTheGood <maarten@degoedeweb.nl>
2020-03-09 14:16:46 +0100

    Init

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Versions.Android.compileSdk)
+
+    dexOptions {
+        javaMaxHeapSize = "2g"
+    }
+
+    defaultConfig {
+        minSdkVersion(Versions.Android.minSdk)
+        targetSdkVersion(Versions.Android.targetSdk)
+
+        applicationId = "nl.hr.projectrage"
+
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    sourceSets["main"].java.srcDir("src/main/myJava")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}")
+
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    object Android {
+        const val compileSdk = 29
+        const val targetSdk = 29
+        const val minSdk = 21
+    }
+
+    const val kotlin = "1.3.70"
+}
