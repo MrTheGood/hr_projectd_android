46e65adf37ec11faeeac84940500184b8945364d
MrTheGood <maarten@degoedeweb.nl>
2020-06-11 21:41:04 +0200

    Add Unit Tests file

---
+++ app/src/test/java/nl/hr/projectrage/WordAnalysisTest.kt
@@ -0,0 +1,24 @@
+package nl.hr.projectrage
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by maartendegoede on 11/06/2020.
+ * Copyright © 2020 Maarten de Goede. All rights reserved.
+ */
+class WordAnalysisTest {
+
+    @Test
+    fun divideIntoSyllables() {
+    }
+
+    @Test
+    fun divideIntoWords() {
+    }
+
+    @Test
+    fun divideIntoPhonetics() {
+    }
+}
